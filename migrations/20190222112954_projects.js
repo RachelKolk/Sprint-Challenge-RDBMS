d43689049fff8c826f5638de889a2bed7a55eb21
RachelKolk <rach.m.obrien@gmail.com>
2019-02-22 11:36:44 -0500

    projects migration

---
+++ migrations/20190222112954_projects.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('projects', function(tbl) {
+        tbl.increments();
+
+        tbl.string('name', 128)
+        .notNullable();
+
+        tbl.text('description', 255)
+        .notNullable();
+
+        tbl.boolean('completed')
+        .defaultTo(false);
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('projects');
+};
