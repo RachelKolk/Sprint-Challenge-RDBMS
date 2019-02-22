5f04f27a86aa1f167d3c4db6bc4b1b8582a8b957
RachelKolk <rach.m.obrien@gmail.com>
2019-02-22 11:27:43 -0500

    knex config, db config, server & index config

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet);
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Hello, from the RDBMS Sprint Challenge!</h1>`);
+});
+
+module.exports = server;
+
+
